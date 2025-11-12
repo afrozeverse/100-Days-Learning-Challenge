171f18f20825a0b3cc9236682aa0f2160c6f6775
afrozeverse <afroz.laptop@gmail.com>
2025-11-12 09:21:14 +0530

    day37

---
+++ Day37_textColorChanger_python/demo.py
@@ -0,0 +1,20 @@
+import tkinter as tk
+def setTextColor(l1,color):
+    l1.config(fg=color)
+def main():
+    root=tk.Tk()
+    root.geometry("400x400")
+    l1=tk.Label(root,text="Change my color",font=("Arial",20))
+
+    b1=tk.Button(root,text="RED",command=lambda:setTextColor(l1,'red'))
+    b2=tk.Button(root,text="GREEN",command=lambda:setTextColor(l1,'green'))
+    b3=tk.Button(root,text="BLUE",command=lambda:setTextColor(l1,'blue'))
+    
+    l1.pack()
+    b1.pack()
+    b2.pack()
+    b3.pack()
+    root.mainloop()
+
+if __name__=="__main__":
+    main()
